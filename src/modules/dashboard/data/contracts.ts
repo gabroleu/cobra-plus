b3d6df480d5c9714d804be8d8a4ca531a093f177
gabroleu <rgabrielcs@gmail.com>
2026-06-26 00:54:06 -0400

    feat: implementar a gestão de clientes e a navegação de contratos

---
+++ src/modules/dashboard/data/contracts.ts
@@ -0,0 +1,23 @@
+export const contracts = [
+    {
+        id: "1",
+        client: "Gabriel Chaves",
+        amount: "R$ 850,00",
+        dueDate: "10/07/2026",
+        status: "Ativo",
+    },
+    {
+        id: "2",
+        client: "Gabriel Silva",
+        amount: "R$1.500,00",
+        dueDate: "15/07/2026",
+        status: "Ativo",
+    },
+
+    {
+        id: "3",
+        client: "Roberto Gabriel",
+        amount: "R$ 420,00",
+        dueDate: "20/07/2026",
+    }
+]
